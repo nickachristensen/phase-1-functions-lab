f98607f3053da1e49e04152893e32881e4b8634a
Nicholas A Christensen <nickachristensen@gmail.com>
2021-08-10 09:29:30 -0400

    commit tree b8cecbaba9f199a9fbd08f510ce0d1b207d84f11

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Code your solution in this file!
+const hq = 42
+const blocksInFeet = 264
+
+const distanceFromHqInBlocks = function(number) {
+    if (number < 42) {
+        return hq - number;
+    }
+    else {
+    return number - hq;
+    }
+}
+
+// distanceFromHqInBlocks(43);
+
+// distanceFromHqInBlocks(50);
+
+// distanceFromHqInBlocks(34);
+
+const distanceFromHqInFeet = function(number) {
+    if (number < 42){
+        return (hq - number) * blocksInFeet;
+    }
+    else {
+        return (number - hq) * blocksInFeet;
+    }
+}
+
+const distanceTravelledInFeet = function(number, number2) {
+    if (number2 > number) {
+        return (number2 - number) * blocksInFeet;
+    }
+    else {
+        return (number - number2) * blocksInFeet;
+    }
+}
+
+// distanceFromHqInFeet(43, 48);
+
+// distanceTravelledInFeet(50, 60);
+
+// distanceTravelledInFeet(34, 28);
+
+
+function calculatesFarePrice(start, destination) {
+    if (Math.abs((start - destination) * blocksInFeet) < 400) {
+        return 0;
+    }
+    else if (Math.abs((start - destination) * blocksInFeet) > 2000) {
+        return 25;
+    }
+    else if (Math.abs((start - destination) * blocksInFeet) > 2500) {
+        return 'cannot travel that far';
+    }
+    else {
+        return Math.abs(((start - destination) * blocksInFeet) - 400) * .02 }
+    }
+
+
+    
